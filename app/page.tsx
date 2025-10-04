d0b6fc54a2f6e57622eb48fe621998f15d53439b
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-04 02:13:19 +0200

    Shadcn ui setup and test with button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div className='bg-red-500 text-center py-2 text-white'>Homepage</div>
+    <Button>Clique</Button>
   )
 }
 
