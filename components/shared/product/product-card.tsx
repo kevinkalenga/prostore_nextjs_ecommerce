9b83df5ca3970151a4438f0cfac3a8c40cd699e0
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-07 17:38:07 +0200

    Product price component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
+import ProductPrice from "./product-price"
 
 const ProductCard = ({product}:{product: any}) => {
   return (
@@ -25,7 +26,7 @@ const ProductCard = ({product}:{product: any}) => {
              <p>{product.rating} Stars</p>
              {
               product.stock > 0 ? (
-                <p className="font-bold">{product.price}</p>
+               <ProductPrice value={Number(product.price)} />
               ): (
                 <p className="text-destructive">Out of Stock</p>
               )            
