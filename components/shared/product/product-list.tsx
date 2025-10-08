5b93820d96fe7076f0141350f200d1912c0a4f28
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-08 11:57:11 +0200

    Zod validation and Type inference

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "./product-card";
+import { Product } from "@/types";
 
-const ProductList = ({data, title, limit}: {data: any, title?: string; limit?:number}) => {
+const ProductList = ({data, title, limit}: {data: Product[], title?: string; limit?:number}) => {
   
   const limitedData = limit ? data.slice(0, limit) : data
   
@@ -12,7 +13,7 @@ const ProductList = ({data, title, limit}: {data: any, title?: string; limit?:nu
          data.length > 0 ? (
            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
               {
-                limitedData.map((product: any) => (
+                limitedData.map((product: Product) => (
                   <ProductCard key={product.slug} product={product} />
                 ))
               }
